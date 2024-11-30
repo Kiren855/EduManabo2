bab968705867978d34db826c71f560c277042d3a
Nong Ngoc Huan <95275091+Kiren855@users.noreply.github.com>
2024-11-30 14:09:02 +0700

    Create env.js

---
+++ env.js
@@ -0,0 +1,8 @@
+const env = {
+    API_BASE_URL: 'https://user-service.ambitiousbush-9297f82e.southeastasia.azurecontainerapps.io', // Địa chỉ webserver
+    COURSE_SERVICE: 'https://course-service.ambitiousbush-9297f82e.southeastasia.azurecontainerapps.io/course', // Địa chỉ webserver
+    ORDER_SERVICE: 'https://order-service.ambitiousbush-9297f82e.southeastasia.azurecontainerapps.io/order', // Địa chỉ webserver
+    LEARNING_SERVICE: 'https://learning-service.ambitiousbush-9297f82e.southeastasia.azurecontainerapps.io/learning/courses', // Địa chỉ webserver
+};
+
+export default env;
